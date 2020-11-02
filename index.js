6b55517c8af02daab7517e833add05e3546fdb90
Alamgir Munir Qazi <alamgir.munir@techavenue.biz>
2020-11-02 12:02:31 +0500

    updated gitignore

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const cron = require('node-cron'), spawn = require('child_process').spawn;
+// once every day '0 0 0 * * *'
 
+// below job will run every minute
 let dbBackupTask = cron.schedule('* * * * *', () => {
 
     const date = new Date().toISOString().slice(0, 10) + '-'+ Date.now();
     archiveCommand = `--archive=./db/backup/${date}.gz`
 
     let backupProcess = spawn('mongodump', [
-        '--db=ea-reporting-db',
+        '--db=my-db-name',
         archiveCommand,
         '--gzip'
       ]);
